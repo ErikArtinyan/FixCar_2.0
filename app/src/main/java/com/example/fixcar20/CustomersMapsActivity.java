8ff272d8d0a0090117436034aaa5727cdbffcdf4
ArtinyanHayk <145184097+ArtinyanHayk@users.noreply.github.com>
2024-03-15 20:29:24 +0400

    tarorinak???

--- app/src/main/java/com/example/fixcar20/CustomersMapsActivity.java
+++ app/src/main/java/com/example/fixcar20/CustomersMapsActivity.java
@@ -1,12 +1,10 @@
 package com.example.fixcar20;
 
-import static com.example.fixcar20.EvacuatorMapsActivity.ACCURACY_THRESHOLD_METERS;
-import static com.example.fixcar20.EvacuatorMapsActivity.ZOOM_LEVEL;
-
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -19,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
@@ -42,93 +41,88 @@ public class CustomersMapsActivity extends FragmentActivity implements OnMapRead
     private static final float ZOOM_LEVEL = 17.0f;
     private GoogleMap mMap;
     private Circle userCircle;
-    Location lastLocation;
     private LocationManager locationManager;
     private ActivityCustomersMapsBinding binding;
     private Button customerLogoutButton;
     private Button callEvacuatorButton;
-    private String customereID;
-    private LatLng CustomerePosition;
+    private String customerID;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
-    private DatabaseReference CustomereDatabaseRef;
+    private DatabaseReference CustomerDatabaseRef;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
+    private LatLng TODO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         binding = ActivityCustomersMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        customerLogoutButton = (Button)findViewById(R.id.customer_logout_button);
+        customerLogoutButton = findViewById(R.id.customer_logout_button);
         callEvacuatorButton = findViewById(R.id.customer_order_evacuator_button);
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
-        customereID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        CustomereDatabaseRef = FirebaseDatabase.getInstance().getReference().child("CustomersE Requests");
+        customerID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        CustomerDatabaseRef = FirebaseDatabase.getInstance().getReference().child("CustomersE Requests");
 
-
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch mapTypeSwitch = findViewById(R.id.map_type_switch);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        Switch mapTypeSwitch = findViewById(R.id.map_type_switch);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
         customerLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
-                LogoutCustomer();
+                logoutCustomer();
             }
         });
-       callEvacuatorButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v)
-           {
-               //CustomerePosition = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-               //mMap.addMarker(new MarkerOptions().position(CustomerePosition).title("Помогите"));
-               //GeoFire geoFire = new GeoFire(CustomereDatabaseRef);
-               //geoFire.setLocation(customereID, new GeoLocation(lastLocation.getLatitude(),lastLocation.getLongitude()));
 
-          }
-       });
+        callEvacuatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasLocationPermission()) {
+                    LatLng customerPosition = getLastKnownLocation();
+                    if (customerPosition != null) {
+                        mMap.addMarker(new MarkerOptions().position(customerPosition).title("Помогите"));
+                        GeoFire geoFire = new GeoFire(CustomerDatabaseRef);
+                        geoFire.setLocation(customerID, new GeoLocation(customerPosition.latitude, customerPosition.longitude));
+                    } else {
+                        Toast.makeText(CustomersMapsActivity.this, "Местоположение не найдено", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    requestLocationPermission();
+                }
+            }
+        });
 
         mapTypeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Проверяем состояние Switch
                 if (isChecked) {
-                    // Если Switch включен, устанавливаем тип карты на обычный
                     mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                 } else {
-                    // Если Switch выключен, устанавливаем тип карты на спутниковый
                     mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
                 }
             }
         });
     }
 
-
-
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        if (hasLocationPermission()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        } else {
+            requestLocationPermission();
         }
-        mMap.setMyLocationEnabled(true);
     }
 
     @Override
@@ -147,27 +141,75 @@ public class CustomersMapsActivity extends FragmentActivity implements OnMapRead
                 userCircle.remove();
             }
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, ZOOM_LEVEL));
-            ////////////
-            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            DatabaseReference CustomereDatabaseRef = FirebaseDatabase.getInstance().getReference().child("CustomersE Requests");
+        }
+    }
 
-
-            GeoFire geoFire = new GeoFire(CustomereDatabaseRef);
-            geoFire.setLocation(userID, new GeoLocation(location.getLatitude(), location.getLongitude()));
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (hasLocationPermission()) {
+            startLocationUpdates();
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
     }
 
-    private void LogoutCustomer()
-    {
+    private void startLocationUpdates() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+    }
+
+    private boolean hasLocationPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start location updates
+                startLocationUpdates();
+            } else {
+                // Permission denied, show a message or handle it accordingly
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private LatLng getLastKnownLocation() {
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return TODO;
+            }
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastKnownLocation != null) {
+                return new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+            }
+        }
+        return null;
+    }
+
+    private void logoutCustomer() {
         Intent welcomeIntent = new Intent(CustomersMapsActivity.this, WelcomeActivity.class);
         startActivity(welcomeIntent);
         finish();
-
     }
 }
